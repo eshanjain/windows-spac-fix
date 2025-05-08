abb8be610aead8512c50eaf48c7b32f3fafb8a18
eshanjain <eshanjain1000@gmail.com>
2025-05-08 16:29:16 -0700

    Update fill_disk.ps1

--- fill_disk.ps1
+++ fill_disk.ps1
@@ -1,4 +1,11 @@
-# fill_disk.ps1
+# disk_fill.ps1
+
+# Print free space BEFORE
+$drive = Get-PSDrive -Name C
+$freeMBBefore = [math]::Round($drive.Free / 1MB)
+Write-Host "[BEFORE] Free space on C: = $freeMBBefore MB"
+
+# Create 1GB file
 $path = "C:\disk_fill.bin"
 $size = 1GB
 $buffer = New-Object byte[] 1MB
@@ -8,3 +15,8 @@ for ($i = 0; $i -lt ($size / 1MB); $i++) {
 }
 $stream.Close()
 Write-Host "Created 1GB file at $path"
+
+# Print free space AFTER
+$drive = Get-PSDrive -Name C
+$freeMBAfter = [math]::Round($drive.Free / 1MB)
+Write-Host "[AFTER] Free space on C: = $freeMBAfter MB"
